008f9095bc614f00063246b2593951fdd0eceee4
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-06-24 10:06:56 -0300

    Like/dislike/remove questions and end rooms

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
+
 import { AuthContextProvider } from './contexts/AuthContext';
+
+import { AdminRoom } from './pages/AdminRoom';
+import { NewRoom } from './pages/NewRoom';
+import { Home } from './pages/Home';
 import { Room } from './pages/Room';
 
 function App() {
@@ -12,6 +15,8 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
